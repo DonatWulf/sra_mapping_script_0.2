1c950c7dd0be3ccd238846e4bf361a575a3180ea
Donat Wulf <donat.wulf@uni-bielefeld.de>
2021-09-16 22:26:09 +0200

    Update kallisto_mapping_multiple.py Cleanup and some comments

--- kallisto_mapping_multiple.py
+++ kallisto_mapping_multiple.py
@@ -1,7 +1,7 @@
 #link="https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-1/SRR3418027/SRR3418027.1"
 #type="SINGLE"
-Run_ID="DRR016685"
-link_p="DRR016685"
+#Run_ID="DRR016685"
+#link_p="DRR016685"
 #link_p="https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-2/ERR476705/ERR476705.1"
 #type_p="PAIRED"
 #index="solyc4.0.idx"
@@ -10,6 +10,7 @@ import subprocess
 import os
 import time
 
+# Downloads and unpacks SRA files form NCBI
 def unpack_sra_file_new(Run_ID,type_,remove=True):
   if type_=="PAIRED":
     call="/usr/bin/fasterq-dump -e 7 --split-files {SRAFile}".format(SRAFile=Run_ID).split(" ")
@@ -18,35 +19,10 @@ def unpack_sra_file_new(Run_ID,type_,remove=True):
     call="/usr/bin/fasterq-dump -e 7 {SRAFile}".format(SRAFile=Run_ID).split(" ")
     out_file=Run_ID+".fastq"
   subprocess.call(call)#, stderr=os.DEVNULL, stdout=os.DEVNULL
-  #for i in out_file.split(" "):
-  #  subprocess.call(["gzip",i])
-  #out_file_gz=out_file.replace("fastq","fastq.gz")
   return out_file
 
 
-
-
-#def unpack_sra_file(URL,type_,remove=True):
-#  subprocess.call(["wget","-nv","-c",URL], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-#  time.sleep(5)
-#  subprocess.call(["wget","-nv","-c",URL], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-#  
-#  file_name=URL.split("/")[-1]
-#  if not os.path.isfile(file_name):
-#    return "NA"
-#  if type_=="PAIRED":
-#    call="fasterq-dump -e 7 --split-files {SRAFile}".format(SRAFile=file_name).split(" ")
-#    out_file=file_name+"_1.fastq"+ " " +file_name+"_2.fastq"
-#  else:
-#    call="fasterq-dump -e 7 {SRAFile}".format(SRAFile=file_name).split(" ")
-#    out_file=file_name+".fastq"
-#  subprocess.call(call, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-#  os.remove(file_name)
-  #for i in out_file.split(" "):
-  #  subprocess.call(["gzip",i])
-  #out_file_gz=out_file.replace("fastq","fastq.gz")
-#  return out_file
-
+# Maps multiple files with Kallisto in SINGLE or PAIRED end mode
 def kallisto_mapping(index,files,type_,output,remove=True):
   print(files)
   for i in files.split(" "):
@@ -64,11 +40,8 @@ def kallisto_mapping(index,files,type_,output,remove=True):
     os.remove(i)
   return "done"
 
-#def download_and_map(index,link_p,type_p,remove=True):
-#  fastqs=unpack_sra_file(link_p,type_p,remove)
-#  kallisto_mapping(index,fastqs,remove)
-#  return "done"
 
+# downloads and maps multiple Runs from SRA
 def download_and_map_multiple(index,link_p,type_p,output,remove=True):
   all_fastq=""
   if type(link_p)==type("a"):
